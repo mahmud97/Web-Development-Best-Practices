e8e35114d65667613b2cb2570fe4165c38c3a5a8
mahmudul islam <mahmudulislam97@gmail.com>
2020-07-04 19:27:32 +0600

    Create JqueryAnimationExample.js

---
+++ JqueryAnimationExample.js
@@ -0,0 +1,16 @@
+
+ $(document).ready(function(){
+$('#fixture-use').change(function(){
+ if($(this).val() == "other")
+ {
+  $('#fixture-use-txt').show();
+ }
+ else
+ {
+ $('#fixture-use-txt').hide();
+ 
+ }
+
+})
+
+})
